587494ba0eb8a1764f651e45dcbb4a8c73157b4a
Ilia Mirkin <ilia@yext.com>
2010-07-15 02:47:14 -0400

    Import changes from gae2django examples

--- settings.py
+++ settings.py
@@ -1,52 +1,98 @@
-# Copyright 2008 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Django settings for django_gae2django project.
 
-"""Minimal Django settings."""
+# NOTE: Keep the settings.py in examples directories in sync with this one!
 
 import os
 
-APPEND_SLASH = False
-DEBUG = os.environ['SERVER_SOFTWARE'].startswith('Dev')
-INSTALLED_APPS = (
-    'codereview',
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
 )
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'dev.db'       # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/static/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'el@4s$*(idwm5-87teftxlksckmy8$tyo7(tm!n-5x)zeuheex'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
 MIDDLEWARE_CLASSES = (
-    #'firepython.middleware.FirePythonDjango',
-    'google.appengine.ext.appstats.recording.AppStatsDjangoMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.http.ConditionalGetMiddleware',
-    'codereview.middleware.AddUserToRequestMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'gae2django.middleware.FixRequestUserMiddleware',
+    'rietveld_helper.middleware.AddUserToRequestMiddleware',
+    'django.middleware.doc.XViewMiddleware',
 )
-ROOT_URLCONF = 'urls'
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
-TEMPLATE_DEBUG = DEBUG
+
+ROOT_URLCONF = 'rietveld_helper.urls'
+
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
-    )
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    )
-FILE_UPLOAD_HANDLERS = (
-    'django.core.files.uploadhandler.MemoryFileUploadHandler',
 )
-FILE_UPLOAD_MAX_MEMORY_SIZE = 1048576  # 1 MB
 
-MEDIA_URL = '/static/'
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'gae2django',
+    'rietveld_helper',
+    'codereview',
+)
 
-RIETVELD_INCOMING_MAIL_ADDRESS = ('reply@%s.appspotmail.com'
-                                  % os.getenv('APPLICATION_ID'))
+AUTH_PROFILE_MODULE = 'codereview.Account'
+LOGIN_REDIRECT_URL = '/'
 
-UPLOAD_PY_SOURCE = os.path.join(os.path.dirname(__file__), 'upload.py')
+# This won't work with gae2django.
+RIETVELD_INCOMING_MAIL_ADDRESS = None
