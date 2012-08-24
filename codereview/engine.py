dc8d0466b8a05daec07493d5bc4c2552ecb9af90
Ilia Mirkin <ilia@yext.com>
2012-08-24 00:15:08 -0400

    Get rid of the dirty tracking. No matter what, the state is dirty, so we should print out the final line.

--- codereview/engine.py
+++ codereview/engine.py
@@ -376,16 +376,14 @@ def _GenerateTriples(old_lines, new_lines, ignore_whitespace):
       prev_index = 0
       for index in xrange(i2 - i1):
         eq = "equal" if old_lines[i1 + index] == new_lines[j1 + index] else "replace"
-        dirty = True
         if prev is None:
           prev = eq
         elif eq != prev:
           yield prev, old_lines[i1+prev_index:i1+index], new_lines[j1+prev_index:j1+index]
           prev_index = index
           prev = eq
-          dirty = False
-      if dirty:
-        yield prev, old_lines[i1+prev_index:i1+index+1], new_lines[j1+prev_index:j1+index+1]
+      assert prev
+      yield prev, old_lines[i1+prev_index:i1+index+1], new_lines[j1+prev_index:j1+index+1]
     else:
       yield tag, old_lines[i1:i2], new_lines[j1:j2]
 
